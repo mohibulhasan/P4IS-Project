dad2ecb5d9e7fec21a852b4c5b6a4b8da9c7bc58
mohibulhasan <mohibul-hasan@outlook.com>
2025-06-06 21:54:02 +0100

    creating get and post methods(class) in views.py

--- backend/backendproject/backendApp/serializer.py
+++ backend/backendproject/backendApp/serializer.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import *
+from . models import *
 
 class ReactSerializer(serializers.ModelSerializer):
     class Meta:
--- backend/backendproject/backendApp/views.py
+++ backend/backendproject/backendApp/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from . models import *
+from . serializer import *
+from rest_framework.response import Response
 
-# Create your views here.
+
+
+class ReactView(APIView):
+    def get(self, request):
+        employees = Employee.objects.all()
+        serializer = ReactSerializer(employees, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = ReactSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=201)
+        return Response(serializer.errors, status=400)
