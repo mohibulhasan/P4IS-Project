113568f296e863253acd72d9bea0eb09f217042f
mohibulhasan <mohibul-hasan@outlook.com>
2025-06-18 20:46:25 +0100

    Trying to work with CRUD in frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,45 +1,74 @@
 //import logo from './logo.svg';
 //import './App.css';
 
-import axios from 'axios';
-import React from 'react';
+import axios from "axios";
+import React from "react";
 
 class App extends React.Component {
-  state = { details: [], };
+  state = { details: [] };
 
   componentDidMount() {
-
-    axios.get('http://localhost:8000/employees/')
-      .then(response => {
+    axios
+      .get("http://localhost:8000/employees/")
+      .then((response) => {
         this.setState({ details: response.data }); // Store fetched employees in state
       })
-      .catch(error => {
+      .catch((error) => {
         console.error("There was an error fetching the employee data!", error);
-      })
-    }
+      });
+  }
+
   render() {
     return (
-      <div className='Container' style={{ padding: '40px' }}>
+      <div className="Container" style={{ padding: "40px" }}>
         <h1>Bangladesh Submarine Cables PLC</h1>
         <hr />
-        <ul>
+        <table>
+          <thead>
+            <tr>
+              <th>First Name</th>
+              <th>Last Name</th>
+              <th>Phone Number</th>
+              <th>Email</th>
+              <th>Department</th>
+              <th>Actions</th>
+            </tr>
+
+            {this.state.details.map((detail, index) => (
+              <tr key={index}>
+                <td>{detail.first_name} </td>
+                <td>{detail.last_name}</td>
+                <td>{detail.phone_number}</td>
+                <td>{detail.email}</td>
+                <td>{detail.department}</td>
+                <td>
+                  <button onclick>Edit</button>
+                  <button onclick>Delete</button>
+                </td>
+              </tr>
+            ))}
+          </thead>
+        </table>
+        {/* <ul>
           {this.state.details.map((detail, index) => (
             <li key={index}>
-              <h2>{detail.first_name} {detail.last_name}</h2>
+              <h2>
+                {detail.first_name} {detail.last_name}
+              </h2>
               <p>Phone: {detail.phone_number}</p>
               <p>Email: {detail.email}</p>
               <p>Department: {detail.department}</p>
             </li>
           ))}
-        </ul>
+        </ul> */}
       </div>
     );
-// function App() {
-//   return (
-//     <div className="App">
-//       <h1>Bangladesh Submarine Cables PLC</h1>
-//     </div>
-//   );
+    // function App() {
+    //   return (
+    //     <div className="App">
+    //       <h1>Bangladesh Submarine Cables PLC</h1>
+    //     </div>
+    //   );
   }
-} 
+}
 export default App;
